83cd76f3c9b1b47eea5eef06b4fe50d3485de8ab
tkach <tkach@DESKTOP-5GL5C3O>
2026-02-04 12:58:57 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RegentHealth
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class User
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Surname { get; set; }
+	public string Password { get; set; }
+	public string Email { get; set; }
+
+	public UserRole Role { get; set; }
+}
+
+public enum UserRole
+{
+    Admin,
+    Client
+}
